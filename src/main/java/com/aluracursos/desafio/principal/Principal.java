ced4a2aad0d3ff8226da963170248995cd8c54fd
juanse951 <juanse951@gmail.com>
2024-11-12 22:33:09 -0500

    se genera un top 10 de los libros mas descargados con la ayuda de .stream

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -1,7 +1,11 @@
 package com.aluracursos.desafio.principal;
 
+import com.aluracursos.desafio.model.Datos;
+import com.aluracursos.desafio.model.DatosLibros;
 import com.aluracursos.desafio.service.ConsumoAPI;
 import com.aluracursos.desafio.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
 
@@ -13,5 +17,15 @@ public class Principal {
     public void muestraElMenu(){
         var json = consumoAPI.obtenerDatos(URL_BASE);
         System.out.println(json);
+        var datos = conversor.obternerDatos(json, Datos.class);
+        System.out.println(datos);
+
+        //Top 10 libros más descargados
+        System.out.println("Top 10 libros más descargados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
     }
 }
