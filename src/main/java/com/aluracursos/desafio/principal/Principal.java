2ef275f80f7b043cb303112850532c8de1bd72fd
juanse951 <juanse951@gmail.com>
2024-11-12 22:48:47 -0500

    busqueda por nombre implementada con stream

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -6,10 +6,15 @@ import com.aluracursos.desafio.service.ConsumoAPI;
 import com.aluracursos.desafio.service.ConvierteDatos;
 
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Principal {
 
+    private Scanner teclado = new Scanner(System.in);
+
     private static final String URL_BASE = "https://gutendex.com/books/";
+
     private ConsumoAPI consumoAPI = new ConsumoAPI();
 
     private ConvierteDatos conversor = new ConvierteDatos();
@@ -27,5 +32,20 @@ public class Principal {
                 .limit(10)
                 .map(l -> l.titulo().toUpperCase())
                 .forEach(System.out::println);
+
+        //Busqueda de libros por nombre
+        System.out.println("Ingrese el nombre del libro que desea buscar: ");
+        var tituloLibro = teclado.nextLine();
+        json = consumoAPI.obtenerDatos(URL_BASE + "?search=" + tituloLibro.replace(" ","+"));
+        var datosBusqueda = conversor.obternerDatos(json, Datos.class);
+        Optional<DatosLibros> libroBuscado = datosBusqueda.resultados().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(tituloLibro.toUpperCase()))
+                .findFirst();
+        if(libroBuscado.isPresent()){
+            System.out.println("Libro encontrado: ");
+            System.out.println(libroBuscado.get());
+        } else {
+            System.out.println("Libro no encontrado.");
+        }
     }
 }
